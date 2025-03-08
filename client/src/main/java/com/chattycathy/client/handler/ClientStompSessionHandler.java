4447a5e38ed289fdb7fe58216cde4114f38e35fc
Cryptizism <cryptizism@protonmail.com>
2025-03-08 14:17:03 +0000

    Project Restructure, Server and Client Bootstrapped

---
+++ client/src/main/java/com/chattycathy/client/handler/ClientStompSessionHandler.java
@@ -0,0 +1,49 @@
+package com.chattycathy.client.handler;
+
+import com.chattycathy.client.model.Model;
+import io.micrometer.common.lang.NonNullApi;
+import io.micrometer.common.lang.Nullable;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Type;
+
+@NonNullApi
+@Component
+@Slf4j
+public class ClientStompSessionHandler extends StompSessionHandlerAdapter {
+
+    @Override
+    public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+        log.info("Connected to WebSocket server");
+        log.debug("Connected successfully to session {}, headers: {}", session, connectedHeaders);
+
+        session.subscribe("/topic/ping", this);
+
+        Model model = new Model("Client", "Hi Chatty Cathy Server");
+
+        session.send("/app/ping", model);
+    }
+
+    @Override
+    public void handleException(StompSession session, @Nullable StompCommand command, StompHeaders headers, byte[] payload, Throwable exception) {
+        log.error("Error: {}", exception.getMessage());
+    }
+
+
+    @Override
+    public void handleFrame(StompHeaders headers, @Nullable Object payload) {
+        if (payload instanceof Model model) {
+            log.info("<{}>: {}", model.getName(), model.getMessage());
+        }
+    }
+
+    @Override
+    public Type getPayloadType(StompHeaders headers) {
+        return Model.class;
+    }
+}
---
+++ client/src/main/java/com/chattycathy/client/handler/commands/Exit.java
@@ -0,0 +1,28 @@
+package com.chattycathy.client.handler.commands;
+import java.util.List;
+
+
+public class Exit implements Command {
+    /**
+     * This method gets the name of the command.
+     * @return the string needed as an input to run the command.
+     */
+    @Override
+    public String getName() {
+        return "/exit";
+    }
+
+
+    /**
+     * This method executes the command, and throws an exception if it fails
+     *
+     * @param parameter is a list of strings,
+     *                  representing further inputs that can be used for commands.
+     *                  for this command parameter should be null
+     */
+    @Override
+    public void execute(List<String> parameter) {
+        System.exit(0);
+    }
+}
+
