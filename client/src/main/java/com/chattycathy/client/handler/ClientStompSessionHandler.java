28c46686e4cfa9a15bccb0a03ae56f7a08ae329e
Cryptizism <cryptizism@protonmail.com>
2025-03-09 22:11:43 +0000

    Added javadocs to ClientStompSessionHandler

--- client/src/main/java/com/chattycathy/client/handler/ClientStompSessionHandler.java
+++ client/src/main/java/com/chattycathy/client/handler/ClientStompSessionHandler.java
@@ -12,11 +12,17 @@ import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Type;
 
+/**
+ * A custom session handler which deals with: subscribing, sending and listening to topics on the server
+ */
 @NonNullApi
 @Component
 @Slf4j
 public class ClientStompSessionHandler extends StompSessionHandlerAdapter {
 
+    /**
+     * A custom implementation that runs after connecting that subscribes to the needed events
+     */
     @Override
     public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
         log.info("Connected to WebSocket server");
@@ -29,12 +35,17 @@ public class ClientStompSessionHandler extends StompSessionHandlerAdapter {
         session.send("/app/ping", model);
     }
 
+    /**
+     * A custom implementation that handles errors when the payload converts
+     */
     @Override
     public void handleException(StompSession session, @Nullable StompCommand command, StompHeaders headers, byte[] payload, Throwable exception) {
         log.error("Error: {}", exception.getMessage());
     }
 
-
+    /**
+     * A custom implementation that handles incoming messages
+     */
     @Override
     public void handleFrame(StompHeaders headers, @Nullable Object payload) {
         if (payload instanceof Model model) {
@@ -42,6 +53,9 @@ public class ClientStompSessionHandler extends StompSessionHandlerAdapter {
         }
     }
 
+    /**
+     * Provides the type of payload this handler will deal with
+     */
     @Override
     public Type getPayloadType(StompHeaders headers) {
         return Model.class;
