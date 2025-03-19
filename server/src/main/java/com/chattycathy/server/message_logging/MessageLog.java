0dd00d1bf1e960888a47113c3b534463303d0590
Bella <2770706h@student.gla.ac.uk>
2025-03-19 14:14:21 +0000

    added javadoc

--- server/src/main/java/com/chattycathy/server/message_logging/MessageLog.java
+++ server/src/main/java/com/chattycathy/server/message_logging/MessageLog.java
@@ -7,6 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * Handles all message logging behaviour including file handling
+ */
 @Slf4j
 @AllArgsConstructor
 public class MessageLog {
