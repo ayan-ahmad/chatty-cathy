e0cb56ce40edf7bacb57ad5afed6377596967d5b
Cryptizism <cryptizism@protonmail.com>
2025-03-20 23:15:50 +0000

    Added post queues on serverside #34

--- server/src/main/java/com/chattycathy/server/controller/MessageController.java
+++ server/src/main/java/com/chattycathy/server/controller/MessageController.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.stereotype.Controller;
 
@@ -18,9 +19,10 @@ import org.springframework.stereotype.Controller;
 public class MessageController {
 
     MessageLog messageLog;
+    SimpMessagingTemplate simpMessagingTemplate;
 
     /**
-     * Logs then returns and sends a message to all clients
+     * Logs then returns and sends a message to clients listening to main and a user-specific topic/posts endpoint
      * @param message This is the message that is sent by a client and logged then received on server-side
      * @return We return a message of our choice, since this is an app destination this will be sent to everyone
      * @see com.chattycathy.server.config.WebSocketConfig#configureMessageBroker(MessageBrokerRegistry)
@@ -28,8 +30,9 @@ public class MessageController {
      */
     @MessageMapping("/main")
     @SendTo("/topic/main")
-    public Message main(Message message) {
+    public Message post(Message message) {
         messageLog.logMessage(message);
+        simpMessagingTemplate.convertAndSend("/topic/posts/"+message.getUserName(), message);
 
         return message;
     }
