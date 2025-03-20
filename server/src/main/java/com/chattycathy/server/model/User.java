bb1f09e884b94ea64dd1663548ba6469f39c22fa
Bella <2770706h@student.gla.ac.uk>
2025-03-20 17:44:00 +0000

    created User entity

---
+++ server/src/main/java/com/chattycathy/server/model/User.java
@@ -0,0 +1,24 @@
+package com.chattycathy.server.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "User")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userId")
+    private int userId;
+
+    @Column(name = "userName")
+    private String userName;
+}
