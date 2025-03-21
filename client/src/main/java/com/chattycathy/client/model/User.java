909a5cb212de9356db8dd57c5c9700f63c64cce0
Andrew Mckelvey <2843209M@student.gla.ac.uk>
2025-03-21 15:34:38 +0000

    Implemented validation on usernames including, stopping username having spaces, being too long, and containing the string 'server'

--- client/src/main/java/com/chattycathy/client/model/User.java
+++ client/src/main/java/com/chattycathy/client/model/User.java
@@ -18,9 +18,33 @@ public class User {
      * @param scanner takes input for userName
      */
     public User(Scanner scanner) {
+
+        boolean nameNonEmpty;
+        boolean validLength;
+        boolean noSpaces;
+        boolean notServer;
+        boolean validInput;
+
         do {
             log.info("Please enter a username: ");
             userName = scanner.nextLine();
-        } while (userName.isEmpty());
+
+            nameNonEmpty = !userName.isEmpty();
+            validLength = userName.length() <= 30;
+            noSpaces = !userName.contains(" ");
+            notServer = !userName.toLowerCase().contains("server");
+
+            if (!validLength) {
+                log.info("Username must be 30 or less characters");
+            }
+            if (!noSpaces) {
+                log.info("Username cannot contain spaces");
+            }
+            if (!notServer) {
+                log.info("Username cannot be 'server'");
+            }
+
+            validInput = nameNonEmpty && validLength && noSpaces && notServer;
+        } while (!validInput);
     }
 }
