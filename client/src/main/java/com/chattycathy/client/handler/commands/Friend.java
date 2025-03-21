3419b89a90a2007e17f936054eeabae4f1567f2c
Dannielle Villanueva <6874781+Emenopi@users.noreply.github.com>
2025-03-21 11:05:38 +0000

    feat(): add friend comment

---
+++ client/src/main/java/com/chattycathy/client/handler/commands/Friend.java
@@ -0,0 +1,46 @@
+package com.chattycathy.client.handler.commands;
+
+import com.chattycathy.client.model.User;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class Friend implements Command {
+    public Friend() {
+    }
+
+    /**
+     * This method gets the name of the command.
+     * @return the string needed as an input to run the command.
+     */
+    @Override
+    public String getName() {
+        return "/friend";
+    }
+
+    /**
+     * informative message about the commands purpose and how to use it.
+     * @return string describing the command.
+     */
+    @Override
+    public String getDescription() {
+        return String.format("%s @[username]- Adds [username] to friends list", this.getName());
+    }
+
+    /**
+     * This method executes the command, adding the user to the friends list.
+     * @param parameter is a list of strings,
+     *                  representing further inputs that can be used for commands.
+     *                  for this command parameter should be a username
+     */
+    @Override
+    public void execute(List<String> parameter) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter("src/main/resources/friendsList.csv", true))) {
+            writer.println(parameter.getFirst());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
